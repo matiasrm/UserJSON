e3dc8448b3eb277d2d1bf5ac81b2cf12c197b3f0
Matias Vidoni <matiasrmontenegro@gmail.com>
2021-06-22 17:43:22 -0300

    Initial Commit

---
+++ UserJ/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UserJ
+//
+//  Created by Mati Montenegro on 22/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UserJ/UserJApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserJApp.swift
+//  UserJ
+//
+//  Created by Mati Montenegro on 22/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct UserJApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
